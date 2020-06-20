791597d872f445f68b92fc0f8cc8263301be651a
Sebastian Suwala <sebastian.suwala1@gmail.com>
2020-06-20 13:58:36 +0200

    SummaryReport implemented and can now be created; started implementing GetDataFromExcel

---
+++ src/main/java/pl/edu/agh/mwo/commodore64/GetDataFromExcel.java
@@ -0,0 +1,18 @@
+package pl.edu.agh.mwo.commodore64;
+
+import org.apache.poi.ss.usermodel.Workbook;
+
+public class GetDataFromExcel
+{
+	private Workbook SummaryReport;
+	
+	public GetDataFromExcel(Workbook SummaryReport)
+	{
+		this.SummaryReport = SummaryReport;
+	}
+	
+	public void populateSummaryReport(String path)
+	{
+		
+	}
+}
--- src/main/java/pl/edu/agh/mwo/commodore64/Main.java
+++ src/main/java/pl/edu/agh/mwo/commodore64/Main.java
@@ -1,10 +1,25 @@
 package pl.edu.agh.mwo.commodore64;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
 public class Main
 {
 	public static void main(String[] args)
 	{
-		//tworzenie tabeli zbiorczej
+		String summaryReportPath = "Raport zbiorczy.xls";
+		SummaryReport summaryReport = new SummaryReport(summaryReportPath);
+		summaryReport.create();
+		try
+		{
+			Workbook SummaryWorkbook = WorkbookFactory.create(new File(summaryReportPath));
+			GetDataFromExcel dataGetter = new GetDataFromExcel(SummaryWorkbook);
+		} catch (EncryptedDocumentException | IOException e) {e.printStackTrace();}
+		
 		//"Dane zosta³y wczytane"
 		//stworzenie Workbooka do tabeli zbiorczej, który bêdzie przekazywany do funkcji
 		
