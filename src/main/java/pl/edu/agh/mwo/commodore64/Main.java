c791777586c7c51513be50db4df6adee01037922
Joanna Nodzynska <joanna.nodzynska@gmail.com>
2020-06-20 15:43:03 +0200

    added intro

--- src/main/java/pl/edu/agh/mwo/commodore64/Main.java
+++ src/main/java/pl/edu/agh/mwo/commodore64/Main.java
@@ -4,9 +4,10 @@ public class Main
 {
 	public static void main(String[] args)
 	{
+		String path = Intro.welcomePage();
 		//tworzenie tabeli zbiorczej
 		//stworzenie Workbooka do tabeli zbiorczej, kt�ry b�dzie przekazywany do funkcji
 		//usuni�cie tabeli zbiorczej
-		PathCommander.getPath("E:/OneDrive/Documents");
+		PathCommander.getPath(path);
 	}
 }
