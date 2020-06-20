fc8642622b3dc4437499b82d3f882283abde4aec
Sebastian Suwala <sebastian.suwala1@gmail.com>
2020-06-20 12:23:47 +0200

    Main class implemented

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main
+{
+	public static void main(String[] args)
+	{
+		//tworzenie tabeli zbiorczej
+		//stworzenie Workbooka do tabeli zbiorczej, który bêdzie przekazywany do funkcji
+		//usuniêcie tabeli zbiorczej
+	}
+}
