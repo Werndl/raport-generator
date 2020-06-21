692c1ddf217ac27669695a9669dcb6f4ceaa8d1d
Joanna Nodzynska <joanna.nodzynska@gmail.com>
2020-06-21 14:12:13 +0200

    Report 3 implemented | issue with long name to be fixed

--- src/main/java/pl/edu/agh/mwo/commodore64/Main.java
+++ src/main/java/pl/edu/agh/mwo/commodore64/Main.java
@@ -10,7 +10,7 @@ public class Main
 		ReportsSelector reportSelector = new ReportsSelector();
 		reportSelector.SelectReport();*/
 
-		PathCommander.getPath("C:/Users/Acer/Downloads/dane");
+		PathCommander.getPath("E:\\jnodz\\Documents\\IT\\PROJEKT\\dane");
 		
 		/*for(Task t : PathCommander.getDataModel())
 		{
@@ -21,8 +21,11 @@ public class Main
 			System.out.println("h " + t.getProject());
 			System.out.println("h " + t.getPerson());
 		}*/
+		Report3 report = new Report3(PathCommander.getDataModel(), "2018", "Damian Kowal");
+		Report5 report1 = new Report5(PathCommander.getDataModel(), "Projekt 2");
+		Report1 report2 = new Report1(PathCommander.getDataModel(), "2018");
 		
-		Report5 report = new Report5(PathCommander.getDataModel(), "Projekt2");
-		//Report1 report = new Report1(dataGetter.getDataModel(), "2018");
+		//TO FIX - issue with long name -???
+		//Report3 report3 = new Report3(PathCommander.getDataModel(), "2018", "Katarzyna Mara");
 	}
 }
