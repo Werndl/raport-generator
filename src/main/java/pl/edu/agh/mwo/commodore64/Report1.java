f71867f81bec7153de06a1418ee4d9d3a1588551
Grzegorz Wolski <gwolski93@gmail.com>
2020-06-21 03:13:37 +0200

    Report version 0.5 printReport implementation

--- src/main/java/pl/edu/agh/mwo/commodore64/Report1.java
+++ src/main/java/pl/edu/agh/mwo/commodore64/Report1.java
@@ -5,6 +5,8 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.Collection;
+import java.util.Iterator;
 
 public class Report1 {
 
@@ -28,10 +30,21 @@ public class Report1 {
     
     public void fillReport() {
     	
+    	for (Task i: filtredDatas) {
+    		for (String x: reportDatas) {
+    			
+    		}
+    	}
+    	
     	
     }
 
     public void printReport() {
+    	
+    	Collection toPrint = reportDatas.entrySet();
+    	Iterator element = entrySet.iterator();
+    	while(element.hasNext())
+    	System.out.println(element.next());
     	
     }
     
