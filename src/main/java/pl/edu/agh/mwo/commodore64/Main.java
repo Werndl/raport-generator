df32c1221d2f4ca897e8f01b5599217e70f8fb1e
Joanna Nodzynska <joanna.nodzynska@gmail.com>
2020-06-21 15:52:03 +0200

    Report3 final version - added month sorting and unique employees

--- src/main/java/pl/edu/agh/mwo/commodore64/Main.java
+++ src/main/java/pl/edu/agh/mwo/commodore64/Main.java
@@ -22,10 +22,10 @@ public class Main
 			System.out.println("h " + t.getPerson());
 		}*/
 		Report3 report = new Report3(PathCommander.getDataModel(), "2018", "Damian Kowal");
-		Report5 report1 = new Report5(PathCommander.getDataModel(), "Projekt 2");
-		Report1 report2 = new Report1(PathCommander.getDataModel(), "2018");
+		//Report5 report1 = new Report5(PathCommander.getDataModel(), "Projekt 2");
+		//Report1 report2 = new Report1(PathCommander.getDataModel(), "2018");
 		
-		//TO FIX - issue with long name -???
-		Report3 report3 = new Report3(PathCommander.getDataModel(), "2018", "Katarzyna Mara ");
+		Report3 report2 = new Report3(PathCommander.getDataModel(), "2018", "Kamil Kosa");
+		Report3 report3 = new Report3(PathCommander.getDataModel(), "2018", "Katarzyna Mara");
 	}
 }
--- src/main/java/pl/edu/agh/mwo/commodore64/Report3.java
+++ src/main/java/pl/edu/agh/mwo/commodore64/Report3.java
@@ -8,11 +8,11 @@ public class Report3 {
 
 	private ArrayList<Task> projectWorkers = new ArrayList<>();
 	private TreeMap<String, Double> workingHours = new TreeMap<>();
-	
-	private boolean checkEmployee(ArrayList<Task> dataModel, String year,String employee) {
-		
-		for(Task i: dataModel) {
-			if(i.getPerson().equals(employee)) {
+
+	private boolean checkEmployee(ArrayList<Task> dataModel, String year, String employee) {
+
+		for (Task i : dataModel) {
+			if (i.getPerson().equals(employee)) {
 				return true;
 			}
 		}
@@ -23,20 +23,22 @@ public class Report3 {
 	public Report3(ArrayList<Task> dataModel, String year, String employee) {
 		System.out.println("++++++++++++++++++++++++++++++++++++++++++");
 		System.out.println(employee + " | Szczegółowy wykaz pracy za rok " + year);
-		if(checkEmployee(dataModel, year, employee)) {
-		for (Task i : dataModel) {
-			System.out.println(i.getPerson() + " - " + i.getMonth() + " - " + i.getProject());
-			if (i.getYear().equals(year)) {
-				//System.out.println(i.getPerson() + " - " + i.getMonth() + " - " + i.getProject());
-				if (i.getPerson().contains(employee)) {
-					projectWorkers.add(i);
-					//System.out.println(i.getPerson() + " - " + i.getMonth() + " - " + i.getProject());
+		if (checkEmployee(dataModel, year, employee)) {
+			for (Task i : dataModel) {
+				// for checking
+				// System.out.println(i.getYear() + " - " + i.getPerson() + " - " + i.getMonth()
+				// + " - " + i.getProject());
+				if (i.getYear().equals(year)) {
+
+					if (i.getPerson().equals(employee)) {
+						projectWorkers.add(i);
+					}
 				}
+
 			}
+			fillReport();
+			printReport();
 		}
-		}
-		fillReport();
-		printReport();
 		System.out.println("++++++++++++++++++++++++++++++++++++++++++");
 	}
 
@@ -44,11 +46,10 @@ public class Report3 {
 		for (Task t : projectWorkers) {
 			String key = makeCompoundKey(t.getMonth(), t.getProject());
 			if (workingHours.containsKey(key)) {
-				
+
 				double dayHours = workingHours.get(key) + Double.parseDouble(t.getHours());
 				workingHours.replace(key, dayHours);
-			} 
-			else {
+			} else {
 				workingHours.put(key, Double.parseDouble(t.getHours()));
 			}
 		}
@@ -57,28 +58,30 @@ public class Report3 {
 	private void printReport() {
 		double sum = 0;
 		int index = 1;
-		System.out.printf("%-10s %-20s %-20s %-10s\n", "Lp", "Miesiac","Projekt", "Godziny [h]");
+		System.out.printf("%-10s %-20s %-20s %-10s\n", "Lp", "Miesiac", "Projekt", "Godziny [h]");
 
 		for (Map.Entry<String, Double> entry : workingHours.entrySet()) {
 			String key = entry.getKey();
-			//String key = "month;test";
 			Double hours = entry.getValue();
-			
+
 			String[] keyValues = key.split(";");
 			String month = keyValues[0];
-			String[] monthName = {"Styczeń","Luty","Marzec","Kwiecień",
-					"Maj","Czerwiec","Lipiec","Sierpień","Wrzesień",
-					"Pażdziernik","Listopad","Grudzień"};
+			String[] monthName = { "Styczeń", "Luty", "Marzec", "Kwiecień", "Maj", "Czerwiec", "Lipiec", "Sierpień",
+					"Wrzesień", "Pażdziernik", "Listopad", "Grudzień" };
 			String project = keyValues[1];
 
-			System.out.printf("%-10s %-20s %-20s %-10s\n", index, monthName[Integer.parseInt(month)-1], project, hours);
+			System.out.printf("%-10s %-20s %-20s %-10s\n", index, monthName[Integer.parseInt(month) - 1], project,
+					hours);
 
 			index++;
 		}
 
 	}
-	
+
 	String makeCompoundKey(String month, String project) {
-		  return month + ";" + project;
+		if (month.length() > 1) {
+			return month + ";" + project;
 		}
+		return "0" + month + ";" + project;
+	}
 }
