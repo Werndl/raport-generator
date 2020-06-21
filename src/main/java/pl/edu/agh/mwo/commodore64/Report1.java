68df2f52f32298ef8402aa1b4e3a1bf96d7789ec
Grzegorz Wolski <gwolski93@gmail.com>
2020-06-21 02:39:17 +0200

    Report version 0.4

--- src/main/java/pl/edu/agh/mwo/commodore64/Report1.java
+++ src/main/java/pl/edu/agh/mwo/commodore64/Report1.java
@@ -1,5 +1,41 @@
 package pl.edu.agh.mwo.commodore64;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class Report1 {
 
+	private TreeMap<String, Double> reportDatas = new TreeMap<String, Double>();
+	private ArrayList<Task> filtredDatas = new ArrayList<Task>();
+	private double temp;
+	
+	
+	
+    public Report1(ArrayList<Task> tasks, int yearFilter){	
+    for (Task i: tasks) {
+    	if(i.getYear() == yearFilter) {
+    		filtredDatas.add(i);
+    	}
+    	fillReport();
+    	printReport();
+    }
+    
+    
+    }
+    
+    public void fillReport() {
+    	
+    	
+    }
+
+    public void printReport() {
+    	
+    }
+    
+    
+	
+	
 }
