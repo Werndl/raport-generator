62d443b98d320e436f71018a1cfa430a457a7801
Sebastian Suwala <sebastian.suwala1@gmail.com>
2020-06-21 07:47:51 +0200

    Task code cleanup

--- src/main/java/pl/edu/agh/mwo/commodore64/Task.java
+++ src/main/java/pl/edu/agh/mwo/commodore64/Task.java
@@ -4,43 +4,18 @@ public class Task
 {
 	private String person, year, month, project, hours;
 
-	public String getPerson() {
-		return person;
-	}
+	public String getPerson() {return person;}
+	public void setPerson(String person) {this.person = person;}
 
-	public void setPerson(String person) {
-		this.person = person;
-	}
+	public String getYear() {return year;}
+	public void setYear(String year) {this.year = year;}
 
-	public String getYear() {
-		return year;
-	}
+	public String getMonth() {return month;}
+	public void setMonth(String month) {this.month = month;}
 
-	public void setYear(String year) {
-		this.year = year;
-	}
+	public String getProject() {return project;}
+	public void setProject(String project) {this.project = project;}
 
-	public String getMonth() {
-		return month;
-	}
-
-	public void setMonth(String month) {
-		this.month = month;
-	}
-
-	public String getProject() {
-		return project;
-	}
-
-	public void setProject(String project) {
-		this.project = project;
-	}
-
-	public String getHours() {
-		return hours;
-	}
-
-	public void setHours(String hours) {
-		this.hours = hours;
-	}
+	public String getHours() {return hours;}
+	public void setHours(String hours) {this.hours = hours;}
 }
