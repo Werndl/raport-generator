f9696f26a56ac7f02ba9b1ea96254ac98790a5b8
Joanna Nodzynska <joanna.nodzynska@gmail.com>
2020-06-26 20:22:34 +0200

    Remove not used condition for excel generator

--- src/main/java/pl/edu/agh/mwo/commodore64/ExcelPrinter.java
+++ src/main/java/pl/edu/agh/mwo/commodore64/ExcelPrinter.java
@@ -8,15 +8,13 @@ public class ExcelPrinter {
 
 	public static void printToExcel(String[] columns, ArrayList<String[]> data) {
 		try {
-			if (data.isEmpty()) {
-				System.out.println("Raport jest pusty - plik nie zostanie wygenerowany");
-			} else {
-				ReportsExtractor.writeExcel(columns, data);
-				System.out.println("Wygenerowany raport jest dostępny w wybranej lokalizacji!");
-			}
+
+			ReportsExtractor.writeExcel(columns, data);
+			System.out.println("Wygenerowany raport jest dostępny w wybranej lokalizacji!");
+
 		} catch (IOException e) {
 			System.out.println("PODANA ŚCIEŻKA JEST BŁĘDNA. Powrót do Menu");
-			//e.printStackTrace();
+			// e.printStackTrace();
 		}
 	}
 
