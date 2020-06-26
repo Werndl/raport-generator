83c7c5fc8facd42fb318e356b5de1f0cf07eb1b6
Joanna Nodzynska <joanna.nodzynska@gmail.com>
2020-06-26 16:49:42 +0200

    fix for Report3 when there is no EEs data for the selected year

--- src/main/java/pl/edu/agh/mwo/commodore64/ExcelPrinter.java
+++ src/main/java/pl/edu/agh/mwo/commodore64/ExcelPrinter.java
@@ -39,9 +39,9 @@ public class ExcelPrinter {
 				case 3:
 					printToExcel(Report3.getColumns(), Report3.getData());
 					break;
-				case 4:
-					printToExcel(Report4.getColumns(), Report4.getData());
-					break;
+//				case 4:
+//					printToExcel(Report4.getColumns(), Report4.getData());
+//					break;
 				case 5:
 					printToExcel(Report5.getColumns(), Report5.getData());
 					break;
@@ -72,11 +72,11 @@ public class ExcelPrinter {
 			}
 			break;
 
-		case 4:
-			if (Report4.getData().isEmpty() || Report4.getData() == null) {
-				return false;
-			}
-			break;
+//		case 4:
+//			if (Report4.getData().isEmpty() || Report4.getData() == null) {
+//				return false;
+//			}
+//			break;
 
 		case 5:
 			if (Report5.getData().isEmpty() || Report5.getData() == null) {
