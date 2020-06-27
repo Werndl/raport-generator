d47fbce35ee7a7d199aae826d23e2c11ec569936
Karolina <karolina.zielinska4@gmail.com>
2020-06-27 02:30:15 +0200

    Add new unit tests

--- src/test/java/IntroTests.java
+++ src/test/java/IntroTests.java
@@ -1,26 +1,14 @@
 import org.junit.Assert;
 import org.junit.Test;
 import pl.edu.agh.mwo.commodore64.Intro;
-import org.hamcrest.Matchers;
 import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 
 
 public class IntroTests {
 
-    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
     private final String testDataFilePath = System.getProperty("user.dir") +
-            "\\src\\test\\java\\testData\\2018\\01\\Damian_Kowal.xls";
-
-//TODO  -Fix
-//    @Test
-//    public void testIntroWithoutParameter() {
-//        String input = "";
-//        InputStream in = new ByteArrayInputStream(input.getBytes());
-//        System.setIn(in);
-//        Assert.assertEquals("", Intro.welcomePage());
-//    }
+            "\\resources\\dane1";
 
     @Test
     public void testIntroWithValidPathToReport() {
